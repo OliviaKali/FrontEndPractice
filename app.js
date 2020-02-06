937b4888f1cf615bb2394493c0358eee1d9170bf
OKali <o.kalinowski27@gmail.com>
2020-02-05 22:34:31 -0500

    added commented out question of javascript tasks

--- app.js
+++ app.js
@@ -6,3 +6,24 @@
 
 // Write a JavaScript program to calculate multiplication 
 //and division of two numbers (input from user).
+
+// Write a JavaScript program to get the website URL (loading page)
+
+// Write a JavaScript function that reverse a number 
+
+// Write a JavaScript function which will take an array of numbers 
+//stored and find the second lowest 
+//and second greatest numbers, respectively. 
+
+// Write a JavaScript program to get the integers in range (x, y).
+
+// Write a merge sort program in JavaScript.
+
+// Write a JavaScript conditional statement to find the largest 
+//of five numbers. Display an alert box to show the result.  
+
+// Write a JavaScript function to validate whether a given 
+//value type is NaN or not.
+
+// Write a JavaScript function to check whether given 
+//value types are same or not.
