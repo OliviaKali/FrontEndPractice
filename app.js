f03ee22f8e5ecb2658013eedfafa76022d4d76a8
OKali <o.kalinowski27@gmail.com>
2020-02-06 09:45:45 -0500

    added multiply and division function

--- app.js
+++ app.js
@@ -5,9 +5,27 @@
 // Write a JavaScript program to calculate multiplication 
 //and division of two numbers (input from user).
 
+var num1;
+var num2;
+
+function multiply (num1, num2) {
+    var result= num1 * num2;
+    console.log(result);
+    return result;
+}
+
+function divide (num1, num2) {
+    var result = num1/num2;
+    console.log(result);
+    return result;
+}
+
+
 // Write a JavaScript program to get the website URL (loading page)
 document.getElementById("websiteURL").innerHTML = 
 "This Site's URL is: " + window.location.href;
+
+// console.log(window.location.href);
 
 // Write a JavaScript function that reverse a number 
 function reverseNum(n) {
@@ -27,6 +45,8 @@ console.log(reverseNum(96525));
 
 // Write a JavaScript conditional statement to find the largest 
 //of five numbers. Display an alert box to show the result.  
+
+window.alert("Hello");
 
 // Write a JavaScript function to validate whether a given 
 //value type is NaN or not.
