278734ceec682bf171f4f041eb7b9ebe4170f1ac
OKali <o.kalinowski27@gmail.com>
2020-02-05 20:04:31 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to display the current day 
+//and time in the following format
+
+
+// Write a JavaScript program to get the current date.
+
+// Write a JavaScript program to calculate multiplication 
+//and division of two numbers (input from user).
